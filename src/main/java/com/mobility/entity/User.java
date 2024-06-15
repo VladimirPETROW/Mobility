fbd86137afbee3e8682dcf29b18e3563789e26ed
VladimirPETROW <61349945+VladimirPETROW@users.noreply.github.com>
2024-06-15 23:42:32 +0300

    Add files via upload

--- src/main/java/com/mobility/entity/User.java
+++ src/main/java/com/mobility/entity/User.java
@@ -1,9 +1,8 @@
 package com.mobility.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -14,4 +13,8 @@ public class User {
     String password;
     @Column(nullable = false)
     Boolean enabled;
+    @ElementCollection
+    @CollectionTable(name = "authorities", joinColumns = @JoinColumn(name = "username", nullable = false))
+    @Column(name = "authority", nullable = false)
+    List<String> authority;
 }
